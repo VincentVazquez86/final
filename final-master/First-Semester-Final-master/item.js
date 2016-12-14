db82d86af8903d4cd256043b175e453d36e2bbb8
VincentVazquez86 <7vvazquez@stpatrick.org>
2016-12-14 11:21:58 -0600

    Finished cart building the base of password.

---
+++ final-master/First-Semester-Final-master/item.js
@@ -0,0 +1,14 @@
+class Item{
+  //Fill in the item class below this comment.
+constructor(n,p,s) {
+this.name = n;
+this.price =p;
+this.shipping =s;
+}
+
+}
+
+//Create your three test items below this comment.
+var mygame = newItem ("NHL", 65,1)
+var mytv = newItem ("TV", 1000,5)
+var myheadset = newItem ("Headset", 25,2)
---
+++ final-master/First-Semester-Final-master/password.js
@@ -0,0 +1,13 @@
+class Password{
+  //The first part of the class.
+constructor(pb,pv){
+  this.publickey = pb;
+  this.privatekey = pv;
+}
+
+  //Instance functions below this comment.
+validPublickey(){
+  
+
+  //Static function below this comment.
+}
